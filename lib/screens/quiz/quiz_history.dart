6ec5020ea3c2fcc23a7b8d4091748fa736dc2e23
Stylianos Theofilou <57598255+StelTheo73@users.noreply.github.com>
2025-01-01 13:16:05 +0200

    imporve empty state quiz history screen

--- lib/screens/quiz/quiz_history.dart
+++ lib/screens/quiz/quiz_history.dart
@@ -42,40 +42,50 @@ class _QuizHistoryScreenState extends State<QuizHistoryScreen> {
               children: <Widget>[
                 const SizedBox(height: 20),
                 if (snapshot.hasData)
-                  for (final QuizScore quizScore in snapshot.data!.reversed)
-                    Card(
-                      elevation: 2.0,
-                      color: Palette.white,
-                      child: ListTile(
-                        title: Row(
-                          children: <Widget>[
-                            RobotoText(
-                              text: AppLocalizations.of(context)!.yourScore,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Palette.black,
-                            ),
-                            const SizedBox(width: 5),
-                            RobotoText(
-                              text:
-                                  '${(quizScore.score / quizScore.noOfQuestions * 100).toStringAsFixed(2)}%',
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ],
-                        ),
-                        subtitle: RobotoText(
-                          text: quizScore.formattedDate,
-                          fontSize: 16,
-                          color: Palette.black,
-                        ),
-                        trailing: RobotoText(
-                          text: '${quizScore.score}/${quizScore.noOfQuestions}',
-                          fontSize: 20,
-                          color: Palette.black,
+                  if (snapshot.data!.isEmpty)
+                    Center(
+                      child: RobotoText(
+                        text: AppLocalizations.of(context)!.quizHistoryEmpty,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  else
+                    for (final QuizScore quizScore in snapshot.data!.reversed)
+                      Card(
+                        elevation: 2.0,
+                        color: Palette.white,
+                        child: ListTile(
+                          title: Row(
+                            children: <Widget>[
+                              RobotoText(
+                                text: AppLocalizations.of(context)!.yourScore,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Palette.black,
+                              ),
+                              const SizedBox(width: 5),
+                              RobotoText(
+                                text:
+                                    '${(quizScore.score / quizScore.noOfQuestions * 100).toStringAsFixed(2)}%',
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                          subtitle: RobotoText(
+                            text: quizScore.formattedDate,
+                            fontSize: 16,
+                            color: Palette.black,
+                          ),
+                          trailing: RobotoText(
+                            text:
+                                '${quizScore.score}/${quizScore.noOfQuestions}',
+                            fontSize: 20,
+                            color: Palette.black,
+                          ),
                         ),
                       ),
-                    ),
                 const SizedBox(height: 20),
               ],
             ),
